8c98799f21385675929b85f1b8cffd662edca7c3
Aditya Pujara <59631311+AdityaPujara23@users.noreply.github.com>
2021-02-05 17:51:37 +1030

    Create test_.py

---
+++ test_.py
@@ -0,0 +1,27 @@
+import validate
+import solve
+import json
+import os
+
+def main():
+    #fetching the sudoku
+    path = os.path.abspath("sudoku.json")
+
+    with open(path) as data_:
+        data = json.load(data_)
+
+    sudoku = data['sudoku']
+
+    # validate the sudoku
+    validation = (validate.run(sudoku)).Validate()
+    if validation is False:
+        print("Sudoku is invalid.")
+    else:
+        print("Sudoku valid. Solved:")
+        solve.solve(sudoku)
+        for list_ in sudoku:
+            print(list_)
+        return
+        
+def test_main():
+  assert 'Sudoku valid. Solved:' in main()
